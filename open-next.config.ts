7f484dc0f39de627005dd37313c3bd8f37923b84
Chinmayee21d <chinmayee@factwise.io>
2026-04-10 13:26:30 +0530

    deployment fixes

--- open-next.config.ts
+++ open-next.config.ts
@@ -3,5 +3,5 @@ import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 export default defineCloudflareConfig({
   // The app doesn't require specific overrides for now, 
   // but defineCloudflareConfig ensures the middleware and workers 
-  // are configured correctly for the Cloudflare Edge.
+  // are configured correctly for the Cloudflare Edg
 });
